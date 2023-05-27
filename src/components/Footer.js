665eab7c7a043b0661b1f67f645685e62877d5b3
engrsayed90 <engrsayed90@gmail.com>
2023-05-27 15:57:20 +0900

    Complete Files

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+      <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to="/" className="hover:underline">Cinemate</Link>. All Rights Reserved.</span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+             {/* <li>
+                  <a href="" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+              </li> */}
+              <li>
+                  <a href="https://www.linkedin.com/in/md-saydur-rahman-7a6405244/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+              </li>
+              <li>
+                  <a href="https://www.facebook.com/saidur.rahman.142892" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Facebook</a>
+              </li>
+              {/* <li>
+                  <a href="" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Youtube</a>
+              </li> */}
+              <li>
+                  <a href="https://github.com/engrsayed90" target="_blank" rel="noreferrer" className="hover:underline">Github</a>
+              </li>
+          </ul>
+      </footer>
+  )
+}
