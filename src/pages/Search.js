7f254b1df97065cdc38bef39ec887d71913f3ee0
engrsayed90 <engrsayed90@gmail.com>
2023-05-27 16:31:59 +0900

    Updated pageTitle

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,14 @@
 import { useSearchParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
+import { useTitle } from "../hooks/useTitle";
 import { Card } from "../components";
 
 export const Search = ({apiPath}) => {
   const [searchParams] = useSearchParams();
   const queryTerm = searchParams.get("q");
-
   const { data: movies } = useFetch(apiPath, queryTerm);
+
+  useTitle(`Search result for ${queryTerm}`);
 
   return (
     <main>
@@ -15,7 +17,7 @@ export const Search = ({apiPath}) => {
       </section>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap">       
-          { movies?.map((movie) => (
+          { movies.map((movie) => (
             <Card key={movie.id} movie={movie} />
           )) }          
         </div>
